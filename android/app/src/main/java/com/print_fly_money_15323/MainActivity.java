c11b7e25b4beb1971caa8962e3628d17429d1990
Crowdbotics <team@crowdbotics.com>
2020-04-02 01:42:47 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/print_fly_money_15323/MainActivity.java
@@ -0,0 +1,15 @@
+package com.print_fly_money_15323;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "print_fly_money_15323";
+    }
+}
